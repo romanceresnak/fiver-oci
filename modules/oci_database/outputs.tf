40f173c8e1a9209feb43ec6682aa1f6a89cc3e1a
romanceresnak <romanceresnak@windowslive.com>
2024-12-04 20:48:32 +0100

    Update outputs.tf

--- modules/oci_database/outputs.tf
+++ modules/oci_database/outputs.tf
@@ -1,1 +1,14 @@
+output "db_system_id" {
+  description = "OCID of the database system"
+  value       = oci_database_db_system.db_system.id
+}
 
+output "db_system_state" {
+  description = "State of the database system"
+  value       = oci_database_db_system.db_system.lifecycle_state
+}
+
+output "db_system_url" {
+  description = "URL of the database system"
+  value       = oci_database_db_system.db_system.connection_strings
+}
