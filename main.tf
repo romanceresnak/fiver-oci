606926798a38209e5b82bbea6328202e4fe9c778
romanceresnak <romanceresnak@windowslive.com>
2024-12-04 20:51:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,23 @@
 
+module "oci_database" {
+  source = "./modules/oci_database"
+
+  region                  = "us-ashburn-1"
+  availability_domain     = "some-ad"
+  compartment_id          = "ocid1.compartment.oc1..example" 
+  db_system_display_name  = "my-database-system"
+  shape                   = "VM.Standard2.1"
+  database_edition        = "ENTERPRISE_EDITION"
+  data_storage_size_in_gbs = 256
+  node_count              = 1
+  storage_management      = "ASM"
+  admin_password          = "YourAdminPassword1!"
+  db_name                 = "CCO"
+  character_set           = "AL32UTF8"
+  ncharacter_set          = "AL16UTF16"
+  db_workload             = "OLTP"
+  db_version              = "19.24.0.0"
+  backup_network_nsg_ids  = []
+  subnet_id               = "ocid1.subnet.oc1..example"
+  ssh_public_keys         = ["ssh-rsa AAAAB3..."]
+}
